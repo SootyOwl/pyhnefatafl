c3ef84e2b138fe63a8098a8c593ee16e9fc600c3
Sooty <7614538+SootyOwl@users.noreply.github.com>
2023-02-13 11:36:06 +0000

    perf: :alembic: Attempt to improve training performance by tweaking params

--- src/alphazero/main.py
+++ src/alphazero/main.py
@@ -1,3 +1,4 @@
+import gc
 import logging
 import sys
 
@@ -6,7 +7,7 @@ import coloredlogs
 from alphazero.coach import Coach
 from hnefatafl.game import HnefataflGame as Game
 from hnefatafl.ai import NNetWrapper as nn
-from alphazero.utils import *
+from alphazero.utils import dotdict
 
 log = logging.getLogger(__name__)
 
@@ -15,23 +16,24 @@ coloredlogs.install(level='INFO')
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
 args = dotdict({
-    'numIters': 1000,
-    'numEps': 20,               # Number of complete self-play games to simulate during a new iteration.
-    'tempThreshold': 15,        #
+    'numIters': 100,
+    'numEps': 25,               # Number of complete self-play games to simulate during a new iteration.
+    'tempThreshold': 15,        # Number of moves before dropping temperature to 0 (ie playing according to the max). Temp is 1 if the number of moves < tempThreshold.
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
-    'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
-    'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
-    'cpuct': 1,
+    'maxlenOfQueue': 100000,    # Number of game examples to train the neural networks.
+    'numMCTSSims': 30,          # Number of games moves for MCTS to simulate.
+    'arenaCompare': 20,         # Number of games to play during arena play to determine if new net will be accepted.
+    'cpuct': 3,                 # The amount of exploration vs exploitation to allow for MCTS. 
+                                #   Larger values will increase exploration of the tree, smaller values will increase exploitation of known good moves. 
+                                #   1-4 is a good range depending on the size of the action space.
 
-    'move_limit': 25,           # maximum number of moves in a game before it is considered a draw
+    'move_limit': 25,           # Maximum number of moves in a game before it is considered a draw. This is to prevent games from going on forever, and to speed up training by exiting games early.
 
-    'checkpoint': './temp/',
-    'load_model': False,
-    'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
+    'checkpoint': f'./checkpoint/{Game.__name__}/',
+    'load_model': True,
+    'load_folder_file': (f'./checkpoint/{Game.__name__}', 'checkpoint_1.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 })
-
 
 def main():
     log.info('Loading %s...', Game.__name__)
@@ -58,4 +60,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # Allow deep tree search by increasing the stack size and recursion limit of the main thread
+    import threading
+    threading.stack_size(1 << 27)
+    sys.setrecursionlimit(1 << 20)
+    thread = threading.Thread(target=main)
+    
+    thread.start()
+    thread.join()
